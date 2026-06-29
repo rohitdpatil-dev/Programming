0173927fb6791adb53c2851e1aa2e8ce003a1438
Rohit Patil <98675841+rohitdpatil-dev@users.noreply.github.com>
2026-06-29 20:52:30 +0530

    Add files via upload

---
+++ Assignments/Assignment 6/Table.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Table(int iNo)
+{
+    int iCnt = 0;
+    int iMult = 1;
+
+    for(iCnt = 1; iCnt <= 10; iCnt++)
+    {
+        iMult = iNo * iCnt;
+        printf("%d\t",iMult);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iFact = 0;
+    
+    printf("Enter your number : ");
+    scanf("%d",&iValue);
+
+    Table(iValue);
+
+    return 0;
+}
